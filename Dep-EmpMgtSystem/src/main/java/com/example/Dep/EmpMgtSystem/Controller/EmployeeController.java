4a10c4921f3527671e50e11b27ab61796bd726c0
Keneriya <kenuts217@gmail.com>
2025-07-14 23:57:30 +0300

    init commit

--- Dep-EmpMgtSystem/src/main/java/com/example/Dep/EmpMgtSystem/Controller/EmployeeController.java
+++ Dep-EmpMgtSystem/src/main/java/com/example/Dep/EmpMgtSystem/Controller/EmployeeController.java
@@ -1,4 +1,22 @@
 package com.example.Dep.EmpMgtSystem.Controller;
 
+import com.example.Dep.EmpMgtSystem.DTO.EmployeeDTO;
+import com.example.Dep.EmpMgtSystem.Entity.Employee;
+import com.example.Dep.EmpMgtSystem.Service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/employees")
 public class EmployeeController {
+    @Autowired
+    private EmployeeService employeeService;
+
+    @PostMapping("/create")
+    public Employee createEmployee(@RequestBody EmployeeDTO dto){
+        return employeeService.createEmployee(dto);
+    }
 }
